33cd9f4a27b4d76d1a16a11339358baeebc37c8a
liangshenghom <793457851@qq.com>
2020-12-18 18:01:14 +0800

    admin

---
+++ spring-boot-admin-client/src/main/java/org/spring/boot/admin/client/adminclient.java
@@ -0,0 +1,13 @@
+package org.spring.boot.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class adminclient 
+{
+    public static void main( String[] args )
+    {
+    	 SpringApplication.run(adminclient.class, args);
+    }
+}
