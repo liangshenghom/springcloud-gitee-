d42cc3bbbf1ac34740f2feb73bf5b3ba588247c8
liangshenghom <793457851@qq.com>
2020-12-22 17:33:20 +0800

    springbootadmin 整合eureka

--- spring-cloud-eureka-client/src/main/java/org/spring/cloud/eureka/client/EurekaClientApp.java
+++ spring-cloud-eureka-client/src/main/java/org/spring/cloud/eureka/client/EurekaClientApp.java
@@ -6,10 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class App 
+public class EurekaClientApp 
 {
     public static void main( String[] args )
     {
-    	  SpringApplication.run(App.class, args);
+    	  SpringApplication.run(EurekaClientApp.class, args);
     }
 }
+  
--- springboot-admin/src/main/java/org/springboot/admin/SpringbootAdmin.java
+++ springboot-admin/src/main/java/org/springboot/admin/SpringbootAdmin.java
@@ -2,8 +2,11 @@ package org.springboot.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+@EnableEurekaServer
 @EnableAdminServer
 @SpringBootApplication
 public class SpringbootAdmin {
